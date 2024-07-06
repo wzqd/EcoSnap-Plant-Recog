c2629f129f67d3b03cfd8dbbc1888fa0a581a7c6
wzqd <1057337859@qq.com>
2024-07-06 17:38:48 +0800

    Initial Commit

---
+++ PlantRecognition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantRecognition
+//
+//  Created by wzqd on 2024/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantRecognition/PlantRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantRecognitionApp.swift
+//  PlantRecognition
+//
+//  Created by wzqd on 2024/7/6.
+//
+
+import SwiftUI
+
+@main
+struct PlantRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
