acfacc871a947852737a63fb65aa038bcc82c089
wzqd <1057337859@qq.com>
2024-07-13 23:06:35 +0800

    WeeklyStepCount

--- PlantRecognition/ContentView.swift
+++ PlantRecognition/ContentView.swift
@@ -17,11 +17,20 @@ struct ContentView: View {
     var body: some View {
         
         
-        healthKitManager.getDailyStepCount() { steps in // This function provides to get the step count of the users.
-                    DispatchQueue.main.async {
-                        print("Step count is:", steps) // Get the step count.
-                    }
+        healthKitManager.getDailyStepCount() { steps in // This function gets the daily step count of the users.
+            DispatchQueue.main.async {
+                    print("Step count is:", steps)
+            }
+        }
+        healthKitManager.getWeeklyStepCount() { steps in // This function gets the weekly step count of the users.
+            DispatchQueue.main.async {
+                for i in 1...7{
+                    print("Day",i ,"Step count is:", steps[i]!)
                 }
+                        
+            }
+        }
+        
         
         return VStack {
             Image(systemName: "globe")
--- PlantRecognition/HealthKitManager.swift
+++ PlantRecognition/HealthKitManager.swift
@@ -9,13 +9,9 @@ import Foundation
 import HealthKit
 
 class HealthKitManager: ObservableObject {
-  static let Instance = HealthKitManager()
+    static let Instance = HealthKitManager()
 
-  var healthStore = HKHealthStore()
-
-//  var stepCountToday: Int = 0
-//  var thisWeekSteps: [Int: Int] = [1: 0, 2: 0, 3: 0,
-//                                   4: 0, 5: 0, 6: 0, 7: 0]
+    var healthStore = HKHealthStore()
 
     
     init() {
@@ -53,6 +49,7 @@ class HealthKitManager: ObservableObject {
                 return
             }
             
+            
             let steps = Int(sum.doubleValue(for: HKUnit.count()))
             
             afterCompletion(steps)
@@ -65,6 +62,63 @@ class HealthKitManager: ObservableObject {
         
         healthStore.execute(query)
     }
+    
+    
+    func getWeeklyStepCount(afterCompletion: @escaping ([Int:Int]) -> Void) {
+        guard let stepCountType = HKQuantityType.quantityType(forIdentifier: .stepCount) else {return}
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+
+        // Find the start date (Monday) of the current week
+        guard let startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: today)) else {
+          print("Failed to calculate the start date of the week.")
+          return
+        }
+
+        // Find the end date (Sunday) of the current week
+        guard let endOfWeek = calendar.date(byAdding: .day, value: 6, to: startOfWeek) else {
+          print("Failed to calculate the end date of the week.")
+          return
+        }
+
+        let predicate = HKQuery.predicateForSamples(
+          withStart: startOfWeek,
+          end: endOfWeek,
+          options: .strictStartDate)
+
+        let query = HKStatisticsCollectionQuery(
+          quantityType: stepCountType,
+          quantitySamplePredicate: predicate,
+          options: .cumulativeSum, // fetch the sum of steps for each day
+          anchorDate: startOfWeek,
+          intervalComponents: DateComponents(day: 1) // interval to make sure the sum is per 1 day
+        )
+
+        query.initialResultsHandler = { _, result, error in
+          guard let result = result else {
+              if let error = error {
+                  print("An error occurred while retrieving step count: \(error.localizedDescription)")
+            }
+            return
+        }
+        
+        var thisWeekSteps: [Int: Int] = [1: 0, 2: 0, 3: 0,
+                                    4: 0, 5: 0, 6: 0, 7: 0]
+
+        result.enumerateStatistics(from: startOfWeek, to: endOfWeek) { statistics, _ in
+            if let quantity = statistics.sumQuantity() {
+              let steps = Int(quantity.doubleValue(for: HKUnit.count()))
+              let day = calendar.component(.weekday, from: statistics.startDate)
+              thisWeekSteps[day] = steps
+            }
+          }
+            
+          afterCompletion(thisWeekSteps)
+        }
+
+        healthStore.execute(query)
+    }
+
     
     
 }
